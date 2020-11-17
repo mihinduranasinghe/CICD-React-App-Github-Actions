a690222d6b18965f4ba5961c0194fba567dfbf40
mihinduranasinghe <mihindu.21523@gmail.com>
2020-11-17 20:24:56 +0530

    feat: published build zip and coverage zip assets

--- release.config.js
+++ release.config.js
@@ -1,3 +1,16 @@
+// module.exports = {
+//     branches: "main",
+//     repositoryUrl:
+//         "https://github.com/mihinduranasinghe/CICD-React-App-Github-Actions",
+//     plugins: [
+//         "@semantic-release/commit-analyzer",
+//         "@semantic-release/release-notes-generator",
+//         "@semantic-release/npm",
+//         "@semantic-release/github",
+//     ],
+// };
+
+// with assets
 module.exports = {
     branches: "main",
     repositoryUrl:
@@ -6,6 +19,14 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/npm",
-        "@semantic-release/github",
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    {path: "build.zip", label: "Build"},
+                    {path: "coverage.zip", label: "Coverage"},
+                ],
+            },
+        ],
     ],
 };
